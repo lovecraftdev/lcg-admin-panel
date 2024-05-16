880b3fe1b58545d125ca50d3b3e3bf4a1dc21a7e
lovecraftdev <168398662+lovecraftdev@users.noreply.github.com>
2024-05-16 15:35:30 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     screens: {
       "3xl": { max: "1918px" },
-      "2xl": { max: "1535px" },
+      "2xl": { max: "1600px" },
       xl: { max: "1279px" },
       md: { max: "1035px" },
       sm: { max: "767px" },
